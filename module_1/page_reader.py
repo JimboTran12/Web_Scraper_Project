6ef12e5ea5912a925053b02bb685a44c8e17f1f2
Jimmy <hungtran2003ti@gmail.com>
2024-03-12 23:29:17 -0500

    class diagram added

--- module_1/page_reader.py
+++ module_1/page_reader.py
@@ -8,8 +8,9 @@ import requests
 class FileReader:
     def __init__(self, input_file_name) -> None:
         self.pages = []
+        self.input_file_name = input_file_name
         try:
-            with open(input_file_name) as file:
+            with open(self.input_file_name) as file:
                 urls = [line.rstrip() for line in file]
             for url in urls:
                 self.pages.append(requests.get(url))
